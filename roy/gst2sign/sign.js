49c1c976ca86e6a413466da2efa345a0ec68bb19
RoyLinux <roy.ros.cobo@gmail.com>
2018-03-13 20:31:36 +0000

    sign server done for now

--- roy/gst2sign/sign.js
+++ roy/gst2sign/sign.js
@@ -10,7 +10,7 @@ const webServerSocket = new WebSocket.Server({ port: port });
 console.log("New server on port "+port);
 
 
-var sockets = [];
+var sockets = [], ids=0;
 
 //--------------txt processing----------------------------
 process.openStdin().addListener("data", function(d) {
@@ -18,11 +18,23 @@ process.openStdin().addListener("data", function(d) {
 	var txt = d.toString().trim();
 
 	if(txt==""){
-		console.log("LIST OF SOCKETS\n------------------------------");
-		for (var i = 0; i < sockets.length; i++) console.log(i+": "+sockets[i].ip);
+		console.log("LIST OF SOCKETS ("+Object.keys(sockets).length+")");
+		console.log("------------------------------");
+		Object.keys(sockets).forEach(function(key){ console.log(key+"="+sockets[key].ip); }); 
 
-	}else if("0"<=txt[0] && txt[0]<="9") sockets[txt[0]].ws.send("~~~From sign server: "+txt.substring(1,txt.length)); 
-	else webServerSocket.broadcast("~~~From sign server: "+txt);
+	}else if("0"<=txt[0] && txt[0]<="9") {
+
+		if("0"<=txt[1] && txt[1]<="9" && sockets[txt[0]+txt[1]]!=undefined) {
+
+			sockets[txt[0]+txt[1]].ws.send("~~~From sign server: "+txt.substring(2,txt.length)); 
+			console.log(">>>Sending to "+txt[0]+txt[1]+":"+txt.substring(2,txt.length));
+
+		}else if(sockets[txt[0]]!=undefined) {
+			sockets[txt[0]].ws.send("~~~From sign server: "+txt.substring(1,txt.length)); 
+			console.log(">>>Sending to "+txt[0]+":"+txt.substring(1,txt.length));
+		}
+
+	}else webServerSocket.broadcast("~~~From sign server: "+txt);
 
 });
 
@@ -31,26 +43,38 @@ process.openStdin().addListener("data", function(d) {
 webServerSocket.on('connection', function(ws, req) {
 
   var ip = req.connection.remoteAddress;
-  sockets.push({ws:ws, ip:ip})
+  var id = ids; ids++;
+  sockets[id]={ws:ws, ip:ip};
 
-  console.log("^^^Conected "+ ip);
-  webServerSocket.broadcast("^^^Conected "+ ip);
+  console.log("^^^Conected "+id+"="+ip);
+  ws.send("Your id is "+id);
+  webServerSocket.broadcast("^^^Conected "+id+"="+ip);
 
-    ws.on('message', function(message) {
-        
-        console.log("<<<Message from "+ip+" :"+message);
-        webServerSocket.broadcast("<<<Message from "+ip+" :"+message);
-    });
+  ws.on('message', function(message) {
+      
+      console.log("<<<Message from "+id+"="+" :"+message);
+      webServerSocket.broadcast(">>>From "+id+"="+" :"+message);
+  });
+
+
+  ws.on('close', function(){
+
+  	console.log("vvvDisconected "+id+"="+ip);
+  	webServerSocket.broadcast("vvvDisconected "+id+"="+ip);
+  	delete sockets[id];
+  })
+
 });
 
 
 
 //----------Functions------------------
 webServerSocket.broadcast = function broadcast(data) {
-  webServerSocket.clients.forEach(function each(client) {
+  webServerSocket.clients.forEach(function (client) {
     if (client.readyState === WebSocket.OPEN) {
       
       client.send(data);
     }
   });
+  console.log(")))Broadcast: "+data)
 };
