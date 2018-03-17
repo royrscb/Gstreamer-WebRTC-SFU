2c84fe6baa719e5aac49553a06867eb01fc222e7
RoyLinux <roy.ros.cobo@gmail.com>
2018-03-17 17:36:22 +0000

    sign perfect

--- roy/iceGstNegotiator/sign/sign.js
+++ roy/iceGstNegotiator/sign/sign.js
@@ -5,20 +5,18 @@ const WebSocket = require('ws');//web socket
 
 
 //------CONSTS-------------------
-const GST_SERVER = 0;
-const SIGNALLING_SERVER = -1;
+const GST_SERVER_ID = 0;
+const SIGNALLING_SERVER_ID = -1;
+
 const BROADCAST = -2;
 
 
 //-------vars----------------------
 var port = 3434;
 var sockets = [], ids=1;//sockets ids starts at 1
-//this signalling server id is -1
-//id of gst server is allways 0
-var gstSocketServer = {socket:null, ip: null, id:GST_SERVER};
 
 
-
+//--------run the server--------------------
 const wss = new WebSocket.Server({ port: port });
 console.log("Signalling server on port "+port);
 
@@ -29,9 +27,11 @@ process.openStdin().addListener("data", function(d) {
 	var txt = d.toString().trim();
 
 	if(txt==""){
+
 		console.log("LIST OF SOCKETS ("+Object.keys(sockets).length+")\n--------------------------------------------");
-    console.log(gstSocketServer.id+" = "+gstSocketServer.ip+"\t\t(socket gst server)");
-		Object.keys(sockets).forEach(function(key){ if(key!=GST_SERVER) console.log(key+" = "+sockets[key].ip); }); 
+    if(sockets[GST_SERVER_ID]==undefined) console.log(GST_SERVER_ID+" = null\t(GST server OFF)");
+    else console.log(GST_SERVER_ID+" = "+sockets[GST_SERVER_ID].ip+"\t(GST server ON)");      
+		Object.keys(sockets).forEach(function(id){ if(id!=GST_SERVER_ID) console.log(id+" = "+sockets[id].ip); }); 
 
 	}else if("0"<=txt[0] && txt[0]<="9") {
 
@@ -40,14 +40,14 @@ process.openStdin().addListener("data", function(d) {
     if(sockets[id]!=undefined){
 
       var txt = txt.substring(1,txt.length);
-      var msg = JSON.stringify({type:"txt",data:"~~~From sign server: "+txt, from:SIGNALLING_SERVER, to:id});
+      var msg = JSON.stringify({type:"txt",data:"~~~From sign server: "+txt, from:SIGNALLING_SERVER_ID, to:id});
 
       sockets[id].socket.send(msg);
       console.log(">>>From:-1 to:"+id+" type:txt data: "+"~~~From sign server: "+txt);
 
     }else console.log("Socket doesnt exist");
 
-	}else wss.broadcast("From sign server: "+txt);
+	}else wss.broadcast(")))From sign server: "+txt);
 });
 
 
@@ -58,8 +58,10 @@ wss.on('connection', function(socket, req) {
   var id = ids; ids++;
   sockets[id]={socket:socket, ip:ip, id:id};
 
-  wss.broadcast("^^^New conected "+id+" = "+ip, socket); console.log("^^^New conected "+id+" = "+ip); 
-  socket.send(JSON.stringify({type:"text", data:"Your id is "+id, from: SIGNALLING_SERVER, to:id}));
+  console.log  ("^^^New conected "+id+" = "+ip); 
+  wss.broadcast("^^^New conected "+id+" = "+ip, id); 
+
+  socket.send(JSON.stringify({type:"text", data:"Your id is "+id, from: SIGNALLING_SERVER_ID, to:id}));
   
 
 
@@ -68,7 +70,7 @@ wss.on('connection', function(socket, req) {
     var data;
 
     try{ data = JSON.parse(msg); }
-    catch(ex){ data = {type:"wrongJSON", data:msg, from:undefined, to:undefined} }
+    catch(ex){ data = {type:"JSON_ERROR", data:msg, from:undefined, to:undefined} }
 
     console.log("________________________________________________________________");
     console.log("<<<Type:"+data.type+" from:"+data.from+" to:"+data.to+" data: "+data.data);
@@ -80,26 +82,27 @@ wss.on('connection', function(socket, req) {
 
       sockets[data.to].socket.send(msg);
 
-    }else if(data.to==SIGNALLING_SERVER && data.type=="gstServerON"){
-
-      wss.broadcast("Gst server active!"); console.log("Gst server active!");
-
-      sockets[GST_SERVER] = {socket:socket, ip:ip, id:GST_SERVER};
-      gstSocketServer = sockets[GST_SERVER];
+    }else if(data.to==SIGNALLING_SERVER_ID && data.type=="gstServerON"){
 
       delete sockets[id];
+      id = GST_SERVER_ID;
 
-    }else if(data.to==BROADCAST) wss.broadcast(")))"+data.data,socket,id);
+      console.log  ("Gst server active!");
+      wss.broadcast("Gst server active!"); 
+
+      sockets[GST_SERVER_ID] = {socket:socket, ip:ip, id:GST_SERVER_ID};
+
+    }else if(data.to==BROADCAST) wss.broadcast(")))"+data.data,id,id);
     console.log("________________________________________________________________");
   });
 
 
   socket.on('close', function(){
 
+    delete sockets[id];
+
   	console.log("vvvDisconected "+id+"="+ip);
   	wss.broadcast("vvvDisconected "+id+"="+ip);
-
-  	delete sockets[id];
   })
 
 });
@@ -108,13 +111,12 @@ wss.on('connection', function(socket, req) {
 
 //----------Private Functions------------------
 //entry normal text
-wss.broadcast = function(msg, exception = null, from=SIGNALLING_SERVER) {
-  wss.clients.forEach(function (client) {
-    if (client.readyState === WebSocket.OPEN) {
+wss.broadcast = function(msg, exception = null, from=SIGNALLING_SERVER_ID) {
+  Object.keys(sockets).forEach(function(id){ 
       
       //to -2 means broadcast
-      if(client!=exception) client.send(JSON.stringify({type:"text", data:msg, from: from, to:BROADCAST}));
-    }
+      if(id!=exception) sockets[id].socket.send(JSON.stringify({type:"text", data:msg, from: from, to:BROADCAST}));
+
   });
   console.log(")))Broadcast: "+msg);
 };
